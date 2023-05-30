05625985a4860a685206e5427ec1bc65a1329bec
suron <suron.maharjan@gmail.com>
2023-05-30 10:29:49 +0545

    [searched music played]

--- app/search/Player.tsx
+++ app/search/Player.tsx
@@ -5,13 +5,11 @@ function Player({song}: {song:string;}) {
     console.log("song src: ", song);
    return (
        <div>
-           <audio >
+           <audio controls autoPlay={true}>
             <source src={song} type="audio/mp3" />
            </audio>
            <h4>Playing now</h4>
-          {/*music search functionality */}
-          {/*player Details here */}
-          {/*plaer controls here */}
+        
        </div>
   )
 }
